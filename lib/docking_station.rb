87761218fd4bef61c32247077bc50cb3a349a894
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-04-16 17:01:31 +0100

    Exercise 12 - add attr_reader

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,8 +1,23 @@
 
 class DockingStation
-  def release_bike
-    Bike.new
+  attr_reader :shed
+
+  def initialize
+    @shed = []
   end
+
+  def release_bike
+      if @shed.empty?
+        fail 'Error: shed empty'
+      else
+        Bike.new
+      end
+  end
+
+  def dock(bike)
+    @shed.push(bike)
+  end
+
 end
 
 class Bike
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -11,4 +11,26 @@ describe DockingStation do
     expect(subject.release_bike.working?).to eq(true)
   end
 
+  it { is_expected.to respond_to(:dock)}
+
+
+  it "dock takes one argument" do
+    expect(subject).to respond_to(:dock).with(1).argument
+  end
+
+  it "stores bike in shed" do
+      subject.dock(Bike.new)
+      expect(subject.shed[-1]).to be_a(Bike)
+  end
+
+  it "adds bike in shed" do
+      shed_length = subject.shed.length
+      subject.dock(Bike.new)
+      expect(subject.shed.length).to eq(shed_length +1)
+  end
+
+  it "raises error if shed empty" do
+    puts subject.release_bike
+    expect{subject.release_bike}.to raise_error 'Error: shed empty'
+  end
 end
